ad86afa6003114348e2cebbdc9e220c7fc49802b
Roman Karpov <poma.05@mail.ru>
2020-04-10 18:59:00 +0400

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DefaultModule } from './layouts/default/default.module';
+import { FullwidthModule } from './layouts/fullwidth/fullwidth.module';
+import { LoginComponent } from './modules/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    DefaultModule,
+    FullwidthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
